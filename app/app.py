60467f45078c6fe27c8bee9c118a8c616d01ac50
Brandon Faunce <masterbbud@gmail.com>
2022-09-29 10:50:02 -0400

    Basic Login Stuff

--- app/app.py
+++ app/app.py
@@ -7,9 +7,43 @@ import os
 import psycopg2
 
 from datetime import datetime
- 
+
+from flask_login import LoginManager, login_user
+
+from flask_bcrypt import Bcrypt
+
+
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+app.secret_key = os.environ['SECRET_KEY']
  
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.get(id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user = User.find(request.args.get('username'), request.args.get('password'))
+    if user:
+        login_user(user)
+        return 'Logged in!'
+    else:
+        return 'Incorrect Login Credentials'
+    
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    user = User.create(request.args.get('username'), request.args.get('password'))
+    if user:
+        login_user(user)
+        return 'Signed up and Logged in!'
+    else:
+        return 'Account already exists with that username'
+
 @app.route("/")
 def home_view():
     return "<h1>Welcome to Geeks for Geeks</h1>"
@@ -56,10 +90,68 @@ def add_song():
 def get_songs():
     return sql.select('songs')
 
+@app.route("/get-accounts")
+def get_songs():
+    return sql.select('accounts')
+
 @app.route("/drop-songs")
 def drop_songs():
     return ''
 
+class User:
+
+    users = {}
+
+    def __init__(self, id):
+        self.id = id
+        User.users.update({id: self})
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_authenticated(self):
+        return self.is_active
+
+    @property
+    def is_anonymouse(self):
+        return False
+    
+    def get_id(self):
+        return self.id
+    
+    def set_id(self, id):
+        self.id = id
+    
+    @staticmethod
+    def get(id):
+        return User.users.get(id)
+
+    @staticmethod
+    def find(username, password):
+        # takes a username and password as input and tries to find the account associated
+        # if none, returns None
+        acc = sql.select('accounts', f"username = '{username}'")
+        if not acc:
+            return None
+        pw = acc[0]['password']
+        if bcrypt.check_password_hash(pw, password):
+            return User(acc[0]['id'])
+
+    @staticmethod
+    def create(username, password):
+        # takes a username and password and creates an account
+
+        # first, check if account already exists:
+        acc = sql.select('accounts', f"username = '{username}'")
+        if acc:
+            return None
+        # then, add username and hashed password to sql
+
+        pw = bcrypt.generate_password_hash(password).decode('utf-8')
+        sql.insert('accounts', {'username': username, 'password': pw})
+        return User.find(username, password)
 
 class SQL:
     def __init__(self):
@@ -72,6 +164,7 @@ class SQL:
         self.tables = {} # tablename : {val: type, val: type}
         self.dropTable('songs')
         self.createTable('songs', {'id': 'SERIAL PRIMARY KEY', 'name': 'TEXT NOT NULL'})
+        self.createTable('accounts', {'id': 'SERIAL PRIMARY KEY', 'username': 'TEXT NOT NULL', 'password': 'TEXT NOT NULL'})
         
     def dropTable(self, name):
         self.cur.execute(f"""
@@ -94,7 +187,7 @@ class SQL:
     def createTable(self, name, columns: dict):
         text = ''
         for n, val in columns.items():
-            text += f'{n} {val}\n'
+            text += f'{n} {val},\n'
         self.cur.execute(f"""
         CREATE TABLE IF NOT EXISTS {name} (
             {text});
