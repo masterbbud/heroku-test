7374abca65054e3967475b3e5541253b5f4c5345
Brandon Faunce <masterbbud@gmail.com>
2022-09-29 10:08:54 -0400

    Added commas to table creation

--- other/testcases.py
+++ other/testcases.py
@@ -1,6 +1,8 @@
 import requests
 
-response = requests.get('https://masterbbud-python-test.herokuapp.com/sql-test')
-print(response.text)
+#response = requests.get('https://masterbbud-python-test.herokuapp.com/drop-songs')
+#response = requests.get('https://masterbbud-python-test.herokuapp.com/add-song?name=lucyintheskywithdiamonds')
+response = requests.get('https://masterbbud-python-test.herokuapp.com/add-song?name=lucy')
+response = requests.get('https://masterbbud-python-test.herokuapp.com/get-songs')
 
-response = requests.get('https://masterbbud-python-test.herokuapp.com/add-song?name=testsong')
+print(response.text)
