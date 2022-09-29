3abbbfaeb98ff3eb8f45ffc086b294412eccbdcf
Brandon Faunce <masterbbud@gmail.com>
2022-09-28 21:02:49 -0400

    Returning json

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
  
 app = Flask(__name__)
  
@@ -8,4 +8,16 @@ def home_view():
 
 @app.route("/get-test")
 def get_test():
-    return "Get Response"
+    retDict = {
+        'users': [
+            {
+                'id': 1,
+                'name': 'brandon'
+            },
+            {
+                'id': 6,
+                'name': 'leah'
+            }
+        ]
+    }
+    return jsonify(retDict)
